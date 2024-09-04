5b41e56b9e0162fdffe1bc0322d1f1078256ebae
kojhack <122357318+kojhack@users.noreply.github.com>
2024-09-04 21:35:50 +0000

    Task 3 finish

--- ATMInterface/src/atm/ATM.java
+++ ATMInterface/src/atm/ATM.java
@@ -1,4 +1,21 @@
 package atm;
 
 public class ATM {
+    private BankAccount bankAccount;
+
+    public ATM(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public void withdraw(double montant) {
+        bankAccount.retrait(montant);
+    }
+
+    public void depot(double montant) {
+        bankAccount.depot(montant);
+    }
+
+    public void checkBalance() {
+        System.out.println("Solde courant: " + bankAccount.getSolde());
+    }
 }
--- ATMInterface/src/atm/BankAccount.java
+++ ATMInterface/src/atm/BankAccount.java
@@ -1,4 +1,36 @@
 package atm;
 
 public class BankAccount {
+
+    private double solde ;
+
+    public BankAccount(double solde) {
+        this.solde = solde;
+    }
+
+    public void depot(double montant) {
+        if (montant > 0) {
+            solde += montant;
+            System.out.println("Depôt réussi! Nouveau Solde: " + solde);
+        } else {
+            System.out.println("Montant  du dépôt doit être positif.");
+        }
+    }
+
+    public void retrait(double montant) {
+        if (montant > 0 && montant <= solde) {
+            solde -= montant;
+            System.out.println("Retrait réussi! Nouveau Solde: " + solde);
+        } else {
+            System.out.println("Solde insuffisant ou montant invalide.");
+        }
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
 }
